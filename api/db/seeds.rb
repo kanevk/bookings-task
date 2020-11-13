620f3fbdd756bad037d6a423a9f2e1ba973f5f5e
kanevk <kamen.e.kanev@gmail.com>
2020-11-13 11:12:01 +0200

    Add seeds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Booking.destroy_all
+Space.destroy_all
+
+%w[Hill-one Banana-city Hippoland].each do |name|
+  Space.create!(name: name)
+end
+
+spaces = Space.all
+
+100.times do
+  space = spaces.sample
+  end_date = Date.current.prev_day(rand(1..5))
+
+  Booking.create!(start_date: end_date - 2.days,
+                  end_date: end_date,
+                  description: "Booking for #{space.name}",
+                  space_id: space.id)
+end
